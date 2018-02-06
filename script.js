03d1d1e78f4e28c29c3011d0fb8adae75e08826f
Todd Rader <todd@toddrader.com>
2018-02-06 16:16:02 -0500

    done

---
+++ script.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+$(function() {
+  var tableSelected="";
+$(".reservation").hide();
+  $(".table").click(function() {
+    $(this).toggleClass('available reserved');
+    tableSelected = $(this).attr("class")[0];
+    $(".tableNumberDisplay").text("Table Number: " + tableSelected);
+    $(".reservation").show(1500);
+  });
+  $(".xOut").click(function() {
+    $('.'+tableSelected).toggleClass('available reserved');
+    $(".reservation").hide(1500);
+  });
+  $("button").click(function() {
+    $(".reservation").hide(1500);
+    $('.'+tableSelected).attr('title', "Name: "+ $(".nameInput").val()+" " +"Size of party: "+ $(".numberInput").val());
+    console.log($('.'+tableSelected).attr('title'));
+  });
+  $("table").on("mouseenter", function () {
+    var tableMoused = $(this).attr("class")[0];
+    console.log("mouse enter");
+    $('.'+tableMoused).attr('title');
+  });
+});
+});
